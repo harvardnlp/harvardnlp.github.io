5530767e213c9367e4a75c07c73b12dc10e1d1bf
Sasha Rush <srush@seas.harvard.edu>
2017-04-10 11:54:52 -0400

    .

--- cv/make.sh
+++ cv/make.sh
@@ -1,5 +1,5 @@
 cat ../_data/papers.yaml ../_data/code.yaml > /tmp/all.yaml
 jinja2 cv.tex /tmp/all.yaml --format yaml > /tmp/cv.tex
-xelatex /tmp/cv.tex
+pdflatex /tmp/cv.tex
 jinja2 cv_short.tex /tmp/all.yaml --format yaml > /tmp/cv_short.tex
-xelatex /tmp/cv_short.tex
+pdflatex /tmp/cv_short.tex
